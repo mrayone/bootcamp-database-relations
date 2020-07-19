f1a4923d96bda341de0d65007e6c02902648ca77
Maycon Rayone <maycon.rayone@gmail.com>
2020-07-19 17:17:20 -0300

    criando customer e registrando serviços

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,22 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findCustomerEmail = await this.customersRepository.findByEmail(email);
+
+    if (findCustomerEmail) throw new AppError('This email address are in use.');
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
